4679f7c8c284aa5fd5fd272b57af13c8d2dc4613
Tomas Lopez-Valcarcel <lopez-valcarcel.t@etoncollege.org.uk>
2024-02-06 07:13:30 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Tomas Lopez-Valcarcel on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
